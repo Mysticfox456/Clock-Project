6adf881b1c57bc6fc84abe830a8c65385820622f
Mysticfox456 <125587028+Mysticfox456@users.noreply.github.com>
2023-05-27 20:43:12 -0400

    added .js
    added .js to project
    fixed css link also added moment.js and moment time zones
    added My Current Location as well

---
+++ src/index.js
@@ -0,0 +1,65 @@
+function updateTime() {
+  //Tokyo
+  let tokyoelement = document.querySelector("#tokyo");
+  if (tokyoelement) {
+    let tokyoDateElement = tokyoelement.querySelector(".date");
+    let tokyoTimeElement = tokyoelement.querySelector(".time");
+    let TokyoTime = moment().tz("Asia/Tokyo");
+
+    tokyoDateElement.innerHTML = tokyoDate.format("MMMM	Do YYYY");
+    tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>A</small>]");
+  }
+
+  // Paris
+  let parisElement = document.querySelector("#paris");
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    let parisTime = moment().tz("Europe/Paris");
+
+    parisDateElement.innerHTML = parisTime.format("MMMM	Do YYYY");
+    parisTimeElement.innerHTML = parisTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+  //Honolulu
+  let honoluluElement = document.querySelector("#honolulu");
+  if (honoluluElement) {
+    let honoluluDateElement = honoluluElement.querySelector(".date");
+    let honoluluTimeElement = honoluluElement.querySelector(".time");
+    let honoluluTime = moment().tz("Pacific/Honolulu");
+
+    honoluluElement.innerHTML = honoluluDate.format("MMMM Do YYYY");
+    honoluluTimeElement.innerHTML = honoluluTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+    <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+    </div>
+    <a href="/">All cities</a>
+    `;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
